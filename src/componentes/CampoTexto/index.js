f05d1c1bf98771f121a5741d9dde28688a00148e
lucasc-costa <62854155+lucasc-costa@users.noreply.github.com>
2023-02-06 21:43:59 -0300

    form

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,10 +1,12 @@
 import "./CampoTexto.css"
 
-const CampoTexto = () => {
+const CampoTexto = (props) => {
+    const placeholderModificada = `${props.placeholder}...`
+
     return (
         <div className="campo-texto">
-            <label>Nome</label>
-            <input placeholder="Digite seu nome"/>
+            <label>{props.label}</label>
+            <input placeholder={placeholderModificada}/>
         </div>
     )
 }
