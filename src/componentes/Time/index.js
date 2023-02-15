a789409fa07d9bab03217687049eee40503e0ed3
lucasc-costa <62854155+lucasc-costa@users.noreply.github.com>
2023-02-15 13:39:40 -0300

    colaborador

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -5,11 +5,10 @@ const Time = (props) => {
     const css = {backgroundColor: props.corSecundaria}
     return (
         <section className='time' style={css}>
-            <h3 style={{borderColor: props.corPrimaria}}>
-                {props.nome}
-            </h3>
-            <Colaborador/>
-            <Colaborador/>
+            <h3 style={{borderColor: props.corPrimaria}}>{props.nome} </h3>
+            <div className='colaboradores'>
+                {props.colaboradores.map( colaborador => <Colaborador nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem}/>)}
+            </div>
         </section>
     )
 }
